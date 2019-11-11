49a5778936fc7ba3b09e4c52038fbf6eef3e3342
Joao Vitor <joaovitorqrz03@gmail.com>
2019-11-11 19:53:31 -0200

    Personalizando Tela

--- src/quiz/Tela2.java
+++ src/quiz/Tela2.java
@@ -34,7 +34,7 @@ public class Tela2 extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(255, 102, 0));
+        jPanel1.setBackground(new java.awt.Color(102, 102, 102));
 
         jLabel3.setIcon(new javax.swing.ImageIcon("C:\\Users\\Cliente\\Desktop\\Imagens\\bobeou-perdeu.png")); // NOI18N
 
